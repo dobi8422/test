63dd070aba84df18e81b14b6e4380156bc231942
Dobi <dobi6658@gmail.com>
2022-12-05 09:32:12 +0800

    test2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,5 @@
 <script setup>
 import { ref } from 'vue'
-const timer = ref(0);
-setInterval(() => timer.value++, 1000)
-
 const action = [
   {
     action: "getToken",
@@ -18,28 +15,22 @@ const token = ref();
 const getAppUserToken = (arg) => {
   token.value = arg || "get token fail";
 };
-
-const token2 = ref();
-const getAppUserToken2 = (arg) => {
-  token2.value = arg || "get token fail";
-};
-let messageHandler;
-setTimeout(() => {
-  messageHandler?.postMessage(JSON.stringify(action[0]))
+const getToken = () => {
+  messageHandler?.postMessage(JSON.stringify(action[0]));
   console.log(JSON.stringify(action[0]));
-}, 0);
-setTimeout(() => getAppUserToken2(), 5000);
-setTimeout(() => {
-  messageHandler?.postMessage(JSON.stringify(action[1]))
+}
+const onOffPage = () => {
+  messageHandler?.postMessage(JSON.stringify(action[1]));
   console.log(JSON.stringify(action[1]));
-}, 10000);
+}
 </script>
 
 <template>
   <ul>
-    <li>time: {{ timer }}</li>
     <li>token: {{ token || "nothing" }}</li>
-    <li>token2: {{ token2 || "nothing" }}</li>
+    <hr>
+    <button @click="getToken">action: getToken</button>
+    <button @click="onOffPage">action: offPage</button>
     <hr>
     <li>{{ device }}</li>
     <hr>
